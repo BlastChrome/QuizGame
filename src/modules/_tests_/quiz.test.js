a0bbbd253e73c60918304b474dcccfa15cc501e2
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2023-12-27 08:51:19 -0800

    created functionality for when a user selects an option

--- src/modules/_tests_/quiz.test.js
+++ src/modules/_tests_/quiz.test.js
@@ -18,7 +18,25 @@ describe("Tests for nomal quiz initialization", () => {
   });
 });
 
-describe("Tests for how questions are loaded into the quiz object", () => {
+const mockQuestions = [
+  {
+    question: "What is 1 + 1?",
+    options: ["1", "2", "3", "4"],
+    answer: "2",
+  },
+  {
+    question: "What is 2 + 2?",
+    options: ["1", "2", "3", "4"],
+    answer: "4",
+  },
+  {
+    question: "What is 1 + 2?",
+    options: ["1", "2", "3", "4"],
+    answer: "3",
+  },
+];
+
+describe("Tests for how the questions array is loaded into the quiz object", () => {
   test("should load questions into the question array", () => {
     const quiz = new Quiz();
     quiz.loadAllQuestions(mockQuestions);
@@ -41,28 +59,10 @@ describe("Tests for how questions are loaded into the quiz object", () => {
       quiz.loadAllQuestions([]);
     };
     expect(loadEmptyQuestions).toThrow(
-      Error("Error: Questions cannot be empty!")
+      Error("Error: Questions array cannot be empty or invalid!")
     );
   });
 });
-
-const mockQuestions = [
-  {
-    question: "What is 1 + 1?",
-    options: ["1", "2", "3", "4"],
-    answer: "2",
-  },
-  {
-    question: "What is 2 + 2?",
-    options: ["1", "2", "3", "4"],
-    answer: "4",
-  },
-  {
-    question: "What is 1 + 2?",
-    options: ["1", "2", "3", "4"],
-    answer: "3",
-  },
-];
 
 describe("Tests to validate the structure of questions", () => {
   test("Should validate the structure of a question object", () => {
@@ -87,5 +87,47 @@ describe("Tests to validate the structure of questions", () => {
     const quiz = new Quiz();
     const invalidOptions = { ...mockQuestions[0], options: undefined };
     expect(quiz.validateQuestion(invalidOptions)).toBe(false);
+  });
+});
+
+describe("Test to validate when the user selects an option", () => {
+  test("Should return true if the correct answer is picked", () => {
+    const quiz = new Quiz();
+    quiz.loadAllQuestions(mockQuestions);
+    const correct = "2";
+    expect(quiz.selectOption(correct)).toBe(true);
+  });
+
+  test("Should return false if any incorrect options are selected", () => {
+    const quiz = new Quiz();
+    quiz.loadAllQuestions(mockQuestions);
+    const incorrect_1 = "1";
+    const incorrect_2 = "4";
+    const incorrect_3 = "3";
+    expect(quiz.selectOption(incorrect_1)).toBe(false);
+    expect(quiz.selectOption(incorrect_2)).toBe(false);
+    expect(quiz.selectOption(incorrect_3)).toBe(false);
+  });
+
+  test("Score should increment when the correct answer is picked", () => {
+    const quiz = new Quiz();
+    quiz.loadAllQuestions(mockQuestions);
+    const correct = "2";
+    quiz.selectOption(correct);
+    const newScore = quiz.getScore(); // 1
+    expect(newScore).toBe(1);
+  });
+
+  test("score should remain the same, if the incorrect answer is picked", () => {
+    const quiz = new Quiz();
+    quiz.loadAllQuestions(mockQuestions);
+    const incorrect_1 = "1";
+    const incorrect_2 = "4";
+    const incorrect_3 = "3";
+    quiz.selectOption(incorrect_1);
+    quiz.selectOption(incorrect_2);
+    quiz.selectOption(incorrect_3);
+    const score = quiz.getScore();
+    expect(score).toBe(0);
   });
 });
--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -1,40 +1,43 @@
-const { isArray } = require("lodash");
-
 class Quiz {
   constructor() {
     this.score = 0;
     this.currentQuizIndex = 0;
     this.questions = [];
   }
+
   //getter methods
   getScore() {
     return this.score;
   }
+
   getAllQuestions() {
     return this.questions;
   }
+
   getCurrentQuizIndex() {
     return this.currentQuizIndex;
   }
+
   getCurrentQuestion() {
     return this.questions[this.currentQuizIndex];
   }
 
   loadAllQuestions(array) {
-    if (array.length > 0) {
-      if (this.validateAllQuestions(array)) {
-        this.questions = array;
-      }
+    if (!Array.isArray(array) || array.length === 0) {
+      throw new Error("Error: Questions array cannot be empty or invalid!");
+    }
+    if (this.validateAllQuestions(array)) {
+      this.questions = array;
     } else {
-      throw new Error("Error: Questions cannot be empty!");
+      throw new Error("Error: Invalid question structure detected!");
     }
   }
 
   validateAllQuestions(questionsToValidate) {
-    if (isArray(questionsToValidate)) {
-      questionsToValidate.forEach((question) => {
+    if (Array.isArray(questionsToValidate)) {
+      for (const question of questionsToValidate) {
         if (!this.validateQuestion(question)) return false;
-      });
+      }
     }
     return true;
   }
@@ -52,8 +55,17 @@ class Quiz {
     if (typeof q.answer !== "string" || !q.options.includes(q.answer)) {
       return false;
     }
-
     return true;
+  }
+
+  selectOption(option) {
+    const question = this.getCurrentQuestion();
+    if (option == question["answer"]) {
+      this.incremementScore();
+      return true;
+    } else {
+      return false;
+    }
   }
 
   //class methods
@@ -61,6 +73,9 @@ class Quiz {
     if (this.currentQuizIndex < this.questions.length - 1) {
       this.currentQuizIndex++;
     }
+  }
+  incremementScore() {
+    this.score++;
   }
 }
 
