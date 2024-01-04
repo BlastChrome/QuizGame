0704eef0a11b39f1ff2f1937688af0221f8817e5
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2024-01-04 08:23:32 -0800

    changed formatting of import statements, for compatibility with testing with nodejs, created early mock variables to test for program flow

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import "./scss/main.scss";
+// import "./scss/main.scss";
+// import Logger from "./modules/logger";
+// import Quiz from "./modules/quiz";
+// import data from "./data.json";
+
+const Logger = require("./modules/logger");
+const Quiz = require("./modules/quiz");
+const data = require("./data.json");
+
+const logger = new Logger();
+
+const quiz = new Quiz(logger);
+
+quiz.loadQuiz(data.quizzes[0]);
