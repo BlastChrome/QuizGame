f3c072890aea0c96300fc7b962711db18859dab5
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2024-01-05 09:10:40 -0800

    created a function inside index that controls the flow of the program, will ask a user a question, accept an input and close when the end of the quiz is reached

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-// import "./scss/main.scss";
-// import Logger from "./modules/logger";
-// import Quiz from "./modules/quiz";
-// import data from "./data.json";
-
+const readline = require("readline");
 const Logger = require("./modules/logger");
 const Quiz = require("./modules/quiz");
-const data = require("./data.json");
+const data = require("./test.json");
+
+//create terminal interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const logger = new Logger();
 
 const quiz = new Quiz(logger);
 
 quiz.loadQuiz(data.quizzes[0]);
+
+// Function to ask a question
+function askQuestion() {
+  const question = quiz.getCurrentQuestionObject();
+  if (quiz.getIsComplete()) {
+    rl.close();
+    return;
+  }
+
+  rl.question(question.question + "\n", (answer) => {
+    quiz.selectOption(answer);
+    // Ask the next question
+    quiz.promptUser();
+    askQuestion();
+  });
+}
+
+// Start asking questions
+askQuestion();
