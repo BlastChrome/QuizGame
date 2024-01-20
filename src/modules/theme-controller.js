6a62346b4ffbe196ff30e42ef26e0e426b6ab026
Nicholas Jackson <nicholasjackson5656@gmail.com>
2024-01-20 15:39:59 -0800

    more changes

--- src/modules/theme-controller.js
+++ src/modules/theme-controller.js
@@ -10,17 +10,7 @@ class ThemeController {
       "click",
       this.handlThemeControllerClick
     );
-
-    // this.selectionButtons = document.getElementById("choice");
-    // this.selectionButtons.addEventListener("click", this.handleSelectionClick);
   }
-
-  // handleSelectionClick = (e) => {
-  //   const button = e.target.closest("button");
-  //   console.log(
-  //     Array.prototype.indexOf.call(this.selectionButtons.children, button)
-  //   );
-  // };
 
   handlThemeControllerClick = (e) => {
     const button = e.target.closest("button");
